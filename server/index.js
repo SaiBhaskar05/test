37fbd03c6097c8d1dceb9ba8d21137354eebc3f0
Nanduri Sai Bhaskar <147789704+SaiBhaskar05@users.noreply.github.com>
2025-01-05 10:19:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const startServer = async () => {
             try {
                 const items = await Item.find();
                 res.json({ items });
+                res.json("Hello");
             } catch (error) {
                 res.status(500).json({ message: 'Failed to fetch items' });
             }
